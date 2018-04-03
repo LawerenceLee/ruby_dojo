8468b5ef7de64947c5d9b14da84ff9faa6f2e91d
LawerenceLee <lucifitz.edward@gmail.com>
2018-04-03 16:49:41 -0700

    updated myenumerable

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -4,11 +4,9 @@ module MyEnumerable
             yield x
         end
     end
-  end
+end
 
+include MyEnumerable
 
-  class Array
-     include MyEnumerable
-  end
-  [1, 2, 3, 4].my_each { |i| puts i } # => 1 2 3 4
-  [1, 2, 3, 4].my_each { |i| puts i * 10 } # => 10 20 30 40
+[1, 2, 3, 4].my_each { |i| puts i } # => 1 2 3 4
+[1, 2, 3, 4].my_each { |i| puts i * 10 } # => 10 20 30 40
