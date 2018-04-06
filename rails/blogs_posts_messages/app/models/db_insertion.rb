5b77c5d7068b1d5aad59afd9d435e091e1d0579f
LawerenceLee <lucifitz.edward@gmail.com>
2018-04-05 17:31:00 -0700

    completed first assignment w/ blogs/..

---
+++ rails/blogs_posts_messages/app/models/db_insertion.rb
@@ -0,0 +1,12 @@
+
+
+
+# title and content, blog
+blog1 = Blog.find(1)
+posts_list = [["Range Rovin", "Drivin all over the country"], ["Hp Scotch", "The merger that should have never been"], ['Jumping For Joy', "Its a lifestyle"]]
+
+
+values = posts_list.map { |u| "('#{u[0]}','#{u[1]}',#{blog1.id})" }.join(",")
+ActiveRecord::Base.connection.execute("INSERT INTO posts (title, content, blog_id) VALUES #{values}")
+
+
